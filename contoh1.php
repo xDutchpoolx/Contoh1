c71b233e68d4cc2d8b406e1335eda5e836565e11
xDutchpoolx <nurwarboys@gmail.com>
2022-03-21 22:44:08 +0700

    Upload file

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Nur Hasanah saya tinggal di Kapuk Pasar Timbul saya sangat menyukai bermain basket";
+    
+    }
+}
+?>
